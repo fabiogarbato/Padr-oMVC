15b37a87802961bf226b793791ccb8420b244c54
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2024-02-03 13:11:02 -0300

    Ajustes no front

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,21 +3,25 @@
     <head>
         <meta charset="UTF-8">
         <title>MVC App</title>
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
-        <h1>Padrão MVC - Fabio Garbato</h1>
-        <?php if (isset($user)): ?>
-            <form action="/path/to/your/form/handler" method="post">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($user['name']); ?>" required>
+        <div div class="container">
+            <h1>Padrão MVC - Fabio Garbato</h1>
+                <?php if (isset($user)): ?>
+                    <form action="/path/to/your/form/handler" method="post">
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($user['name']); ?>" required>
 
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
 
-                <input type="submit" value="Save Changes">
-            </form>
-        <?php else: ?>
-            <p>User not found.</p>
-        <?php endif; ?>
+                        <input type="submit" value="Save Changes">
+                    </form>
+                <?php else: ?>
+                <p>User not found.</p>
+            <?php endif; ?>
+        </div>
+        
     </body>
 </html>
